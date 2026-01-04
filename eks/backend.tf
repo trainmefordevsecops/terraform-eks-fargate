c1025716f1c25549d156e70083dcd2919360458d
trainmefordevsecops <trainmefordevsecops@gmail.com>
2026-01-04 14:29:30 +0800

    updated backend file

--- eks/backend.tf
+++ eks/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "tf-state-iac-demo"
     key            = "eks/terraform.tfstate"
     region         = "ap-southeast-1"
-    dynamodb_table = "terraform-state-lock"
     encrypt        = true
     use_lockfile = true
   }
